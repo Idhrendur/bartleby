225dc2bcac666f589712a67beefe25eff276e0ad
Idhrendur <idhrendur@hotmail.com>
2026-05-12 20:11:51 -0700

    Show multiple queries and results.

--- bartleby.js
+++ bartleby.js
@@ -7,15 +7,30 @@ inputArea.onkeydown = function(event)
         let entry = inputArea.value;
         if (entry != "")
         {
+            query.innerHTML = "";
+            inputArea.value = "";
+
             let entryDiv = document.createElement('div');
             entryDiv.className = "entryDiv";
             entryDiv.innerHTML = `${entry}`;
-            bartleby.append(entryDiv);
+            inputDiv.parentNode.insertBefore(entryDiv, inputDiv); 
+            entryDiv.style.maxWidth = "fit-content";
+            entryDiv.style.marginLeft = "auto";
+            entryDiv.style.marginRight = 0;
+            entryDiv.style.fontSize = "100%";
+            entryDiv.style.fontWeight = "normal";
+            entryDiv.style.minHeight = "30px";
+            entryDiv.style.lineHeight = "30px";
+            entryDiv.style.borderRadius = "30px";
+            entryDiv.style.backgroundColor = "#954535";
+            entryDiv.style.verticalAlign = "middle";
 
             let resultsDiv = document.createElement('div');
             resultsDiv.className = "resultsDiv";
             resultsDiv.innerHTML = "I would prefer not to.";
-            query.replaceWith(resultsDiv);
+            inputDiv.parentNode.insertBefore(resultsDiv, inputDiv);
+
+           
         }
     }
 }
